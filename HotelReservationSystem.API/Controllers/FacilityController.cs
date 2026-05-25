c75aa8444520ce9d73e773221f75b6ac8b7ddf0b
Abdelrhman Elsaeed <abdelrhman.ahmed.elsaeed@gmail.com>
2026-05-25 18:53:43 +0300

    Add Facility endpoints and update ViewModel
    Added a nullable `ID` property to `AddFacilityVM` to support facility operations. Updated `FacilityController` with new endpoints for adding, updating, deleting, and retrieving facilities. Integrated MediatR for handling commands and queries. Adjusted `using` directives to include necessary namespaces.

--- HotelReservationSystem.API/Controllers/FacilityController.cs
+++ HotelReservationSystem.API/Controllers/FacilityController.cs
@@ -1,12 +1,16 @@
 ﻿using Application.AutoMapper.Profiles;
 using Application.CQRS.Auth.Command;
+using Application.CQRS.Facility.Command;
+using Application.CQRS.Facility.Queries;
 using Application.CQRS.Reservation.Command;
 using Application.DTOS;
 using Application.DTOS.Auth;
+using Application.DTOS.Facility;
 using Application.DTOS.Reservation;
 using Application.DTOS.User;
 using Application.Enum;
 using Application.ViewModel.Auth;
+using Application.ViewModel.Facility;
 using Application.ViewModel.Receipt;
 using Application.ViewModel.Reservation;
 using MediatR;
@@ -26,6 +30,60 @@ namespace HotelReservationSystem.API.Controllers
             _Mediator = mediator;
         }
 
+        [HttpPost]
+        public async Task<IActionResult> AddFacility([FromBody] AddFacilityVM model, CancellationToken cancellationToken)
+        {
+            var FacilityDto = model.Map<AddFacilityDto>();
+            var result = await _Mediator.Send(new AddFacilityCommand(FacilityDto), cancellationToken);
 
+            if (result.IsSuccess)
+                return Ok(ResponseViewModel<AddFacilityVM>.Success(result.Data.Map<AddFacilityVM>(), result.Message));
+
+            return BadRequest(ResponseViewModel<AddFacilityVM>.Failure(result.ErrorCode, result.Message));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFacility([FromBody] UpdateFacilityVM model, CancellationToken cancellationToken)
+        {
+            var FacilityDto = model.Map<UpdateFacilityDto>();
+            var result = await _Mediator.Send(new UpdateFacilityCommand(FacilityDto), cancellationToken);
+
+            if (result.IsSuccess)
+                return Ok(ResponseViewModel<UpdateFacilityVM>.Success(result.Data.Map<UpdateFacilityVM>(), result.Message));
+
+            if (result.ErrorCode == Application.Enum.ErrorCode.FacilityNotExist)
+                return NotFound(ResponseViewModel<UpdateFacilityVM>.Failure(result.ErrorCode, result.Message));
+
+            return BadRequest(ResponseViewModel<UpdateFacilityVM>.Failure(result.ErrorCode, result.Message));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFacility([FromBody] DeleteFacilityVM model, CancellationToken cancellationToken)
+        {
+            var FacilityDto = model.Map<DeleteFacilityDto>();
+            var result = await _Mediator.Send(new DeleteFacilityCommand(FacilityDto), cancellationToken);
+
+            if (result.IsSuccess)
+                return Ok(ResponseViewModel<bool>.Success(result.Data, result.Message));
+
+            if (result.ErrorCode == Application.Enum.ErrorCode.FacilityNotExist)
+                return NotFound(ResponseViewModel<bool>.Failure(result.ErrorCode, result.Message));
+
+            return BadRequest(ResponseViewModel<bool>.Failure(result.ErrorCode, result.Message));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFacility(int id, CancellationToken cancellationToken)
+        {
+            var result = await _Mediator.Send(new GetFacilityQuery(id), cancellationToken);
+
+            if (result.IsSuccess)
+                return Ok(ResponseViewModel<GetFacilityVM>.Success(result.Data.Map<GetFacilityVM>(), result.Message));
+
+            if (result.ErrorCode == Application.Enum.ErrorCode.FacilityNotExist)
+                return NotFound(ResponseViewModel<GetFacilityVM>.Failure(result.ErrorCode, result.Message));
+
+            return BadRequest(ResponseViewModel<GetFacilityVM>.Failure(result.ErrorCode, result.Message));
+        }
     }
 }

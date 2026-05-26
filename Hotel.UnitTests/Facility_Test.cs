f7f7feab2c59e7ef51195b089cb1b4080e32c860
Abdelrhman Elsaeed <abdelrhman.ahmed.elsaeed@gmail.com>
2026-05-26 18:57:34 +0300

    Add Hotel.UnitTests project and initial test classes
    Added the `Hotel.UnitTests` project to the solution under `/Persistence/`.
    Created `Facility_Test` and `Template_Test` classes with placeholder
    NUnit test methods. Configured `Hotel.UnitTests.csproj` with .NET 10.0
    target framework, nullable reference types, and required NuGet packages.
    Added a project reference to the `Application` project.

---
+++ Hotel.UnitTests/Facility_Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.UnitTests
+{
+    [TestFixture]
+    public class Facility_Test
+    {
+        [Test]
+        public void AddFacilityCommand()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+        }
+    }
+}
---
+++ Hotel.UnitTests/Template_Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.UnitTests
+{
+    [TestFixture]
+    public class Template_Test
+    {
+        [Test]
+        public void MethodName_Scenario_ExpectedOutcome()
+        {
+
+            // Arrange 
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
